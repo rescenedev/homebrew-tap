047f2fc51a9238bd5958507a13f5f71d5e62e4dc
Seongil Park <zihado@gmail.com>
2026-06-07 13:56:55 +0900

    Add markforge cask v0.1.0

---
+++ Casks/markforge.rb
@@ -0,0 +1,21 @@
+cask "markforge" do
+  version "0.1.0"
+  sha256 "98f23954e37e0928830e9c7f8feedf9ac7bd52ed5caeb500422c73c636d6e652"
+
+  url "https://github.com/rescenedev/markforge/releases/download/v#{version}/MarkForge.app.zip"
+  name "MarkForge"
+  desc "Native macOS Markdown viewer & editor built in Rust on GPUI"
+  homepage "https://github.com/rescenedev/markforge"
+
+  app "MarkForge.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/MarkForge.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/MarkForge",
+  ]
+end
