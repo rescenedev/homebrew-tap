00d206a710eca9306ec7286246fe19b438e01c4f
Seongil Park <zihado@gmail.com>
2026-06-23 20:58:05 +0900

    pomodoro: refresh 1.1.0 dmg (remove tomato)

--- Casks/pomodoro.rb
+++ Casks/pomodoro.rb
@@ -1,6 +1,6 @@
 cask "pomodoro" do
   version "1.1.0"
-  sha256 "83a4c0a52d742fc369b26710781b1737f769dc75620eb095c98c2b8cf187711d"
+  sha256 "675b51e0fe2194c93edbd9159e44e3800a6e189ec4f2822230b0b55321611261"
 
   url "https://github.com/rescenedev/pomodoro/releases/download/v#{version}/Pomodoro.dmg"
   name "Pomodoro"
