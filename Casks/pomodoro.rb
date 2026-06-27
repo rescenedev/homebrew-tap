46cff7f07629650e7a86a0dea06dc7b52f46541a
Seongil Park <zihado@gmail.com>
2026-06-27 14:51:36 +0900

    pomodoro 1.2.2 (swipe to switch session)

--- Casks/pomodoro.rb
+++ Casks/pomodoro.rb
@@ -1,6 +1,6 @@
 cask "pomodoro" do
-  version "1.2.1"
-  sha256 "11811d12656f661153aa2c10cecefb7dd32b6130124fe3138db93eeed9562fbc"
+  version "1.2.2"
+  sha256 "0c2cb7523133d0c1b926a56e74140b7aac900a70f506df561b17602322522225"
 
   url "https://github.com/rescenedev/pomodoro/releases/download/v#{version}/Pomodoro.dmg"
   name "Pomodoro"
